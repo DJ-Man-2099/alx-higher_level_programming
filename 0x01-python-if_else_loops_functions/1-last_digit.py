634a75076ed957ce4eb7a67b4bf9d61e09fd6f04
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-03 20:29:51 +0300

    Task 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ try:
     last_digit = int(str(number)[-1])
     if number < 0:
         last_digit = -last_digit
-    what_is_number = "is 0" if last_digit == 0 \
+    what_is_number = "0" if last_digit == 0 \
         else "greater than 5" if last_digit > 5 else "less than 6 and not 0"
     print(f"Last digit of {number} is {last_digit} and is {what_is_number}")
 except ValueError:
