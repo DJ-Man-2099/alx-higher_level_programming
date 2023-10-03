ddacb57bc79a9eafe7278b89cce4a9c77c5a462b
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-03 08:52:24 +0300

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-what_is_number = "zero" if number == 0 \
- else "positive" if number > 0 else "negative"
-print(f"{number} is {what_is_number}")
+for a in range(ord('a'), ord('z') + 1):
+    print(f"{chr(a)}", end="")
