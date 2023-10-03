f98504038154775d8c44ba1636ac0cdfc8303e6d
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-03 23:42:24 +0300

    Task 14

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,30 +13,30 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *before_new_node = *head, *new_node = NULL;
 
-	if (before_new_node->n >= number)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	if (before_new_node == NULL)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->next = before_new_node;
-		new_node->n = number;
 		*head = new_node;
-		return (new_node);
 	}
-	while (before_new_node != NULL)
+	else if (before_new_node->n >= number)
 	{
-		if ((before_new_node->next != NULL && before_new_node->next->n >= number) ||
-			(before_new_node->next == NULL && before_new_node->n < number))
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				break;
-			new_node->next = before_new_node->next;
-			new_node->n = number;
-			before_new_node->next = new_node;
-			break;
-		}
-		before_new_node = before_new_node->next;
+		new_node->next = before_new_node;
+		*head = new_node;
 	}
+	else
+		while (before_new_node != NULL)
+		{
+			if ((before_new_node->next != NULL && before_new_node->next->n >= number) ||
+				(before_new_node->next == NULL && before_new_node->n < number))
+			{
+				new_node->next = before_new_node->next;
+				before_new_node->next = new_node;
+				break;
+			}
+			before_new_node = before_new_node->next;
+		}
 	return (new_node);
 }
