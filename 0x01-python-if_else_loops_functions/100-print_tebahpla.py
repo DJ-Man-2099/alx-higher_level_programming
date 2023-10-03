4a2beb4d56d1d68ea79f7b2160173837f65019fe
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-03 23:49:43 +0300

    Optional Task 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
+isCapital = False
+for a in reversed(range(ord('a'), ord('z') + 1)):
+    a = a if not isCapital else a + (ord('A')-ord('a'))
     print("{0}".format(chr(a)), end="")
+    isCapital = not isCapital
