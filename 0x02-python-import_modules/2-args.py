0371edb859b19c4bbb8a93ad5421d89b15c8961d
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-06 01:16:16 +0300

    Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     num_args = len(args)
-    str_num = f"{num_args} {'argument' if num_args == 1 else \
+    str_num = f"{num_args} {'argument' if num_args == 1 else 
                             'arguments'}{':' if num_args > 0 else '.'}"
     print(str_num)
     for arg in range(num_args):
