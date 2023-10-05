2d6acc731c08edda7725cab43f3f6665408a54d7
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-06 02:33:15 +0300

    Optional Task 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-
-
-def main():
-    a = 10
-    b = 5
-    add_res = add(a, b)
-    sub_res = sub(a, b)
-    mul_res = mul(a, b)
-    div_res = div(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, add_res))
-    print("{:d} - {:d} = {:d}".format(a, b, sub_res))
-    print("{:d} * {:d} = {:d}".format(a, b, mul_res))
-    print("{:d} / {:d} = {:d}".format(a, b, div_res))
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    num_args = len(args)-1
+    if num_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+    sign = args[2]
+    res = 0
+    if ['+', '-', '*', '/'].count(sign) == 0:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if sign == '+':
+        res = add(a, b)
+    if sign == '-':
+        res = sub(a, b)
+    if sign == '*':
+        res = mul(a, b)
+    if sign == '/':
+        res = div(a, b)
+    print(f"{a:d} {sign} {b:d} = {res:d}")
