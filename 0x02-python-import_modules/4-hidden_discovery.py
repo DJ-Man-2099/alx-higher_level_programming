0f92a90545525155c51a21a511bfc471519c79b6
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-06 01:41:24 +0300

    Task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
 
-
-def main():
-    a = 1
-    b = 2
-    c = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, c))
+def no_prefix(value):
+    return not value.startswith("__")
 
 
 if __name__ == "__main__":
-    main()
+    names = list(filter(no_prefix, dir('hidden_4.pyc')))
+    for name in sorted(names):
+        print(name)
