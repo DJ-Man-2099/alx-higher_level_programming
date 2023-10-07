c5b8a7be5f87be1f6c72861bb10f50c1391985e9
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-07 21:45:12 +0300

    Task 9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+def multiple_returns(sentence):
+    str_len, first_char = (0, None)
+    if len(sentence) > 0:
+        str_len, first_char = (len(sentence), sentence[0])
+    return (str_len, first_char)
