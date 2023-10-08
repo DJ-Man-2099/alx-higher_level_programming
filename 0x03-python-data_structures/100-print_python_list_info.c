829fddcdf72e2f9391e1568db3c9e7923f14a4f6
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-08 22:47:37 +0300

    Optional Task 1

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,7 +10,7 @@
 void print_python_list_info(PyObject *p)
 {
 	size_t list_size = PyList_Size(p), i;
-	size_t list_allocated = list_size + Py_SIZE(p);
+	size_t list_allocated = ((PyListObject *)p)->allocated;
 	printf("[*] Size of the Python List = %ld\n", list_size);
 	printf("[*] Allocated = %ld\n", list_allocated);
 	for (i = 0; i < list_size; i++)
