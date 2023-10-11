0c8544d1f82f56a64acd047f0a4f8eb34219d633
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 00:07:54 +0300

    Task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,12 @@
 def best_score(a_dictionary):
     max_key = None
     if a_dictionary is not None:
-        max_value = max(a_dictionary.values())
-        for key in a_dictionary.keys():
-            if a_dictionary[key] == max_value:
-                max_key = key
-                break
+        score_list = list(
+            filter(lambda value: value is not None, a_dictionary.values()))
+        if len(score_list) != 0:
+            max_value = max(score_list)
+            for key in a_dictionary.keys():
+                if a_dictionary[key] == max_value:
+                    max_key = key
+                    break
     return (max_key)
