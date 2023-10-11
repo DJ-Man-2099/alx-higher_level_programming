5c27b076ca23733f6f9559af30ef93b3a266c17f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 00:54:06 +0300

    Task 13

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,12 @@ roman_numerical = {
     "I": 1,
     "V": 5,
     "X": 10,
+    "XL": 40,
+    "XC": 90,
     "L": 50,
     "C": 100,
+    "CD": 400,
+    "CM": 900,
     "D": 500,
     "M": 1000,
 }
@@ -15,7 +19,7 @@ roman_numerical = {
 def roman_to_int(roman_string):
     sum = 0
     if roman_string is not None and isinstance(roman_string, str):
-        for key in ["IV", "IX", "I", "V", "X",
+        for key in ["IV", "IX", "XL", "XC", "CD", "CM", "I", "V", "X",
                     "L", "C", "D", "M"]:
             sum += roman_string.count(key)*roman_numerical[key]
             roman_string = roman_string.replace(key, " ")
