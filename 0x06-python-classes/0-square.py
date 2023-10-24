83727387f9b86520eb12da24b59de95fc4fbc836
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-24 16:13:36 +0300

    Task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+
+__doc__ = '''
+This is the First Task
+'''
+
+
 class Square:
-    '''This is a Square Class
+    __doc__ = '''This is a Square Class
     for Defining Squares
 
     attributes: None
     '''
 
     def __init__(self) -> None:
+        __doc__ = '''This is the init function
+                for init the class
+                '''
         pass
