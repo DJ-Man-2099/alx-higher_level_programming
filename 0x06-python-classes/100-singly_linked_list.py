f8523159d959f155d85f79db528f922da24563e4
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-24 18:08:48 +0300

    Optional Task 1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -77,7 +77,8 @@ class SinglyLinkedList:
                 new_node.next_node = current
                 self.__head = new_node
             else:
-                while current.next_node is not None and current.next_node.data <= value:
+                while current.next_node is not None and \
+                        current.next_node.data <= value:
                     current = current.next_node
                 new_node.next_node = current.next_node
                 current.next_node = new_node
