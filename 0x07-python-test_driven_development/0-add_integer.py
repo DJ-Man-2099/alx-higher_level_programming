1fc98fb4a114f251721d910a1ea16eacbbe6760b
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-27 12:41:03 +0200

    1st Task with Tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
 Module for first task
+Using Tests to check task if successful
+
 """
 
 
 def add_integer(a, b=98) -> int:
     """
-   This Function adds 2 numbers
+    This Function adds 2 numbers
 
     params: a
-            b (optional)"""
+            b (optional)
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
