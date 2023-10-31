aa51c80bd5f84202c95f8c0f4c54e41e7a172390
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-31 15:46:44 +0200

    Optional Task 1

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.invocations += 1
-    string = list("BestSchool" for i in range(magic_string.invocations))
+    magic_string.calls = getattr(magic_string, 'calls', 0) + 1
+    string = list("BestSchool" for i in range(magic_string.calls))
     return ", ".join(string)
-
-
-magic_string.invocations = 0
