ae5256fbb26421b316d24df2c07b6916358d8b47
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-31 09:46:20 +0200

    6th Task with Unit Test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-print("hello")
+class TestMaxInteger(unittest.TestCase):
+    """
+    Class for Unit Testing max_integer
+
+    this class checks for every detail in the function
+    """
+    def test_max(self):
+        """
+        checks if it calculated max correctly
+        """
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+    
+    def test_empty_list(self):
+        """
+        checks if it handles empty lists correctly
+        """
+        self.assertEqual(max_integer([]),None)
+
+if __name__ == '__main__':
+    unittest.main()
