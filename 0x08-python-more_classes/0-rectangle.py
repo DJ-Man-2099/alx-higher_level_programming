faba391faee1955ceb73f6aa45986d5790c96256
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-10-31 09:58:41 +0200

    Task 1

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,12 +4,14 @@
 1st Task Module
 """
 
+
 class Rectangle:
-	"""
-	Class that defines a rectangle
-	"""
-	def __init__(self) -> None:
-		"""
-		init function
-		"""
-		pass
+    """
+    Class that defines a rectangle
+    """
+
+    def __init__(self) -> None:
+        """
+        init function
+        """
+        pass
