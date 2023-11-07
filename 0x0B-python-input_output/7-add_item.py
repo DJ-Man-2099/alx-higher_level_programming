0d3b55e233e3062d42cc69ec91b3cc5f8c78d48b
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-08 01:10:17 +0200

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+8th Task
+"""
+
+
+import sys
+
+
+if __name__ == "__main__":
+    """
+    adds all arguments to a Python list,
+    and then save them to a file
+    """
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    old_list = load_from_json_file("add_item.json")
+    if old_list is None:
+        old_list = []
+    old_list.extend(sys.argv)
+    save_to_json_file("add_item.json", old_list)
