72e9abf4beda1894487bf6b70a1327ebedf5f507
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-13 19:04:36 +0200

    Task 9

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,7 +46,8 @@ class Base:
         writes the JSON string representation of list_objs to a file
         """
         list_to_json = list(
-            map(lambda obj: obj.to_dictionary(), list_objs)) or []
+            map(lambda obj: obj.to_dictionary(), list_objs)) \
+            if list_objs else []
         file_name = f"{cls.__name__}.json"
         with open(file_name, "w") as file:
             file.write(Base.to_json_string(list_to_json))
