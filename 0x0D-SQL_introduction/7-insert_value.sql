392a33295f503a97e6a2208ef9242e87ebb1953b
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-13 23:21:33 +0200

    Task 7 and 8

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-SELECT *
-FROM first_table;
+INSERT INTO first_table(id, name)
+VALUES (89, "first_table");
