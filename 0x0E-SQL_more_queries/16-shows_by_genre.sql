54ba28097fb7f491fc4f7666e14882cc234e5a1f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 11:22:55 +0200

    tASK 17

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT tv_shows.title,
 	tv_genres.name
 from tv_genres
 	JOIN tv_show_genres ON (tv_genres.ID = tv_show_genres.genre_id)
-	JOIN tv_shows ON(tv_shows.id = tv_show_genres.show_id)
+	RIGHT OUTER JOIN tv_shows ON(tv_shows.id = tv_show_genres.show_id)
 ORDER BY tv_shows.title,
 	tv_genres.name;
