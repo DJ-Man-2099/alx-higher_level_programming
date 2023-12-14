66d0dea1522d8698479c2d2409da8a1d1a9a5326
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 08:40:35 +0200

    oPTIONAL

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
