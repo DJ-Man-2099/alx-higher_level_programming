74ef5740ebdd86a7d14b52d81919fac06264488f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 08:32:46 +0200

    chore(sql): convert table to utf8
    Use `hbtn_0c_0` and convert `first_table` to character set utf8mb4 with collation utf8mb4_unicode_ci. This change updates the table's character encoding for better Unicode support.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,3 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
-MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
