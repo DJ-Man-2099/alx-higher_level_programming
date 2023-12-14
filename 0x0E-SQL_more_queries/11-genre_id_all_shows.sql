33377cf0658309510f15fbc51d6807952325c8f7
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 10:56:59 +0200

    Task 12

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- create database if doesn't exist
+SELECT tv_shows.title,
+	tv_show_genres.genre_id
+from tv_shows
+	LEFT OUTER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title,
+	tv_show_genres.genre_id;
