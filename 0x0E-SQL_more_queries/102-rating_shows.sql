b52501212045716c626c9cf9d1065d48bb7bdee2
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 11:41:07 +0200

    Optional

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- create database if doesn't exist
 SELECT tv_shows.title,
-	SUM(tv_show_ratings.rate) as rating
-from tv_shows
+	SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
 	JOIN tv_show_ratings ON (tv_shows.id = tv_show_ratings.show_id)
 GROUP BY tv_shows.title
 ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- create database if doesn't exist
 SELECT tv_genres.name,
-	SUM(tv_show_ratings.rate) as rating
-from tv_shows
+	SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
 	JOIN tv_show_ratings ON (tv_shows.id = tv_show_ratings.show_id)
 	JOIN tv_show_genres ON (tv_shows.id = tv_show_genres.show_id)
 	JOIN tv_genres ON (tv_genres.id = tv_show_genres.genre_id)
