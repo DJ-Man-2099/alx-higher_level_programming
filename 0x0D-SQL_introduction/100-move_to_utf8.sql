1e6478df3ff7b6ca486914107fa27ead5e0b94ca
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 08:33:54 +0200

    refactor(sql): change character set to utf8mb4
    Refactored SQL script to change the character set to utf8mb4 for database hbtn_0c_0 and table first_table. This ensures compatibility with a wider range of characters and improves Unicode support.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,6 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-USE `hbtn_0c_0`
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
