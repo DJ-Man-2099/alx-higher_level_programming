e9ca34539391efbb1ed2eeb7e11abf121e1fddb1
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-14 08:36:05 +0200

    chore(sql): update character set to utf8mb4
    Removed unnecessary database selection and modified table character sets and collation to use utf8mb4_unicode_ci for better Unicode support.

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,3 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-use hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
-MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
