342c1a764e5e29df438b3dff007f467cf6988a70
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-08 18:47:33 +0200

    Task 8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 if (process.argv[2] && !isNaN(process.argv[2])) {
-  console.log('My number: ' + process.argv[2]);
+  for (
+    let index = 0;
+    index < parseInt(process.argv[2]);
+    index++
+  ) {
+    console.log('C is fun');
+  }
 } else {
-  console.log('Not a number');
+  console.log('Missing number of occurrences');
 }
