31503bb834ce14dd6c52ea102f5afababd43c55d
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-13 09:04:56 +0200

    Task 5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-lists all states from the database hbtn_0e_0_usa
+lists all cities from the database hbtn_0e_0_usa
 '''
 
 import MySQLdb
@@ -10,11 +10,11 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     db = MySQLdb.connect("localhost", args[1], args[2], args[3])
-    name = args[4]
     c = db.cursor()
     c.execute(
-        'SELECT * from states WHERE states.name LIKE \
-          BINARY %s ORDER BY states.id', (name,))
+        """SELECT cities.id, cities.name, states.name
+        from cities JOIN states WHERE states.id = cities.state_id
+        ORDER BY cities.id""")
     rows = c.fetchall()
     for row in rows:
         print(row)
