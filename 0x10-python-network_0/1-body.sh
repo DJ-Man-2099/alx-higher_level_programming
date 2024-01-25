6e768fc74bbb5413b7ee5085fa5cbb6714aba2e8
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 00:24:35 +0200

    Fix GET request and response handling

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 # takes in a URL, sends a GET request to the URL, and stores the body and status code in an array
 readarray -t response < <(curl -s -w "\n%{http_code}" "$1") && [ "${response[-1]}" -eq 200 ] && curl -s "$1"
-=======
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"
->>>>>>> 7f5b79be688c6b8b9189682fddf213a830604fca
