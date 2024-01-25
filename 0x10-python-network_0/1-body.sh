03137749e385f24a338ffa4600417bf4b82ae70e
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 00:27:08 +0200

    Task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and stores the body and status code in an array
-readarray -t response < <(curl -s -w "\n%{http_code}" "$1") && [ "${response[-1]}" -eq 200 ] && curl -s "$1"
+curl -sL "$1"
