902c391bcd25eab15925d070baa0b3aab2a063b8
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 17:32:11 +0200

    Task 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,16 +3,13 @@
 Module
 """
 import sys
-import urllib.request as request
-import urllib.parse as parse
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
     body = {
         'email': sys.argv[2]
     }
-    parsed_data = parse.urlencode(body).encode('ascii')
-    req = request.Request(url, parsed_data)
-    with request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
+    with requests.post(url, data=body) as response:
+        body = response.text
         print(body)
