d048e40daf01f2295352c427f265dbb3e2317ee5
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 17:04:52 +0200

    Task 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,6 +5,7 @@ Module
 import sys
 import urllib.request as request
 import urllib.parse as parse
+import urllib.error as error
 
 if __name__ == "__main__":
     url = sys.argv[1]
@@ -13,6 +14,9 @@ if __name__ == "__main__":
     }
     parsed_data = parse.urlencode(body).encode('ascii')
     req = request.Request(url, parsed_data)
-    with request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    try:
+        with request.urlopen(req) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
