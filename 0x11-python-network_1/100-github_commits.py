8f8d148027dd1e3e0f9ac4dbf6e6bfbff6ba1f49
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 19:06:15 +0200

    Optional Task 1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,12 +6,14 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    user_name = sys.argv[1]
-    token = sys.argv[2]
-    url = f"https://api.github.com/users/{user_name}"
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     headers = {
-        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json",
         "X-GitHub-Api-Version": "2022-11-28"
     }
     with requests.get(url, headers=headers) as response:
-        print(response.json().get("id"))
+        for commit in response.json()[:10]:
+            print(commit.get("sha"), end=": ")
+            print(commit.get("commit").get("author").get("name"))
