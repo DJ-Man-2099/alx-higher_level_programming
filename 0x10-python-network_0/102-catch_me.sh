a7e591f71f5141cf00a0f7f2c0f1743ea9fe6ec8
dj-man-2099 <davidmesak@hotmail.com>
2024-01-26 10:54:56 -0800

    Optional Task 3

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body of the response
-curl -sL "0.0.0.0:5000/catch_me"
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" "0.0.0.0:5000/catch_me_3"
+
