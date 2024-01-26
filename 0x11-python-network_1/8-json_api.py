e3019db809e356b18658f107faa9cb72c1b91d86
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 18:23:23 +0200

    Task 9

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,10 +6,16 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
     body = {
-        'email': sys.argv[2]
+        'q': sys.argv[1] if sys.argv[1] else ""
     }
     with requests.post(url, data=body) as response:
-        body = response.text
-        print(body)
+        try:
+            body = response.json()
+            if body == {}:
+                print("No result")
+            else:
+                print(f"[{body.get('id')}] {body.get('name')}")
+        except ValueError:
+            print("Not a valid JSON")
