eff97f56a3c85563c9934bb33c0ec836cc265d86
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 19:44:30 +0200

    Optional Task 1

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+curl -s -o /dev/null -I -2 "%{http_code}" "$1"
