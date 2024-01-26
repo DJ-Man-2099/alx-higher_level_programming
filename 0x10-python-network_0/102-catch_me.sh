fb8b3b0ba1c78b6146ef70474051f6e040abe320
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 20:03:37 +0200

    Optional Task 3

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL and displays the size of the body of the response
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sL "0.0.0.0:5000/catch_me"
