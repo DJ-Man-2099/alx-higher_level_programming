58b0c9ae0ac82fa23740b06ea6d8298467dd1c8a
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-26 17:51:27 +0200

    Task 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         with requests.get(url) as response:
             body = response.text
             print(body)
-    except requests.HTTPError as e:
+    except requests.exceptions.HTTPError as e:
         if e.response is not None and e.response.status_code >= 400:
             print("Error code: {}".format(e.response.status_code))
