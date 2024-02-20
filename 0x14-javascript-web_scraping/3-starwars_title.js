0b69b7d9b082eb41c28551e03f0ea9fa591e2b22
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-20 15:29:45 +0200

    Task 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,12 @@ const { log } = require('console');
 const request = require('request');
 
 request(
-	`https://swapi-api.alx-tools.com/api/films/${process
-		.argv[2]}`,
-	(error, response, body) => {
-  if (error) {
-    log(error);
+  `https://swapi-api.alx-tools.com/api/films/${process
+    .argv[2]}`,
+  (error, response, body) => {
+    if (error) {
+      log(error);
+    }
+    log(JSON.parse(body).title);
   }
-  log(JSON.parse(body)['title']);
-}
 );
