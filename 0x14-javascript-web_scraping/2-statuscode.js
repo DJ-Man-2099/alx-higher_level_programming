ae480b45b9951cb562aad10cb2eaacb1bad8e4f9
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-20 15:07:56 +0200

    Task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const { log } = require('console');
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
+  if (error) {
+    log(error);
+  }
   log('code:', response.statusCode);
 });
