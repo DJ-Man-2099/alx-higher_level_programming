3fd260ec22156bff9a308dc9834c91d9527ed6b5
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-20 16:04:02 +0200

    Task 6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,28 +1,17 @@
 #!/usr/bin/node
 const { log } = require('console');
 const request = require('request');
+const fs = require('fs');
 
 request(
   process.argv[2],
   (error, response, body) => {
     if (error) {
       log(error);
-    } else if (response.statusCode !== 200) {
-      console.log('An error occured. Status code: ' + response.statusCode);
     } else {
-      let count = 0;
-      const id = 18;
-      const movies = (JSON.parse(body)).results;
-      for (const index in movies) {
-        const movie = movies[index];
-        const characters = movie.characters;
-        for (const char in characters) {
-          if (characters[char].includes(id)) {
-            count++;
-          }
-        }
-      }
-      log(count);
+      fs.writeFileSync(process.argv[3], body, {
+        encoding: 'utf8'
+      });
     }
   }
 );
