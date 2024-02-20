61f212b9047f930780e439386ebf68699ec3d6fa
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-20 15:44:14 +0200

    Task 5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,20 @@ const { log } = require('console');
 const request = require('request');
 
 request(
-  'https://swapi-api.alx-tools.com/api/people/18',
+  process.argv[2],
   (error, response, body) => {
     if (error) {
       log(error);
     }
-    log(JSON.parse(body).films.length);
+    let count = 0;
+    let movies = (JSON.parse(body)).results;
+    for (let index = 0; index < movies.length; index++) {
+      let movie = movies[index];
+      if (movie.characters &&
+        movie.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")) {
+        count++;
+      }
+    }
+    log(count);
   }
 );
